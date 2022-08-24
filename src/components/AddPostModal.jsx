426f5c9d46f5327d91c46555b94f964cf82b8813
devcactus <ahamedesrak3@gmail.com>
2022-08-25 02:14:53 +0600

    Fixed uploding issue

--- src/components/AddPostModal.jsx
+++ src/components/AddPostModal.jsx
@@ -1,5 +1,5 @@
-import React, { useState,   } from 'react';
-import { useDispatch,   } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Modal from '@mui/material/Modal';
 import styles from '../style/addPost.module.css';
 import { postActions } from '../store/redux';
@@ -53,33 +53,33 @@ export default function AddPostModal({
         // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        // console.log('Upload is ' + progress + '% done');
+        console.log('Upload is ' + progress + '% done');
         setProgress(progress);
-        // switch (snapshot.state) {
-        //   case 'paused':
-        //     console.log('Upload is paused');
-        //     break;
-        //   case 'running':
-        //     console.log('Upload is running');
-        //     break;
-        // }
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
       },
-      // (error) => {
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      //   switch (error.code) {
-      //     case 'storage/unauthorized':
-      //       // User doesn't have permission to access the object
-      //       break;
-      //     case 'storage/canceled':
-      //       // User canceled the upload
-      //       break;
-      //     // ...
-      //     case 'storage/unknown':
-      //       // Unknown error occurred, inspect error.serverResponse
-      //       break;
-      //   }
-      // },
+      (error) => {
+        //  A full list of error codes is available at
+        //firebase.google.com/docs/storage/web/handle-errors
+        https: switch (error.code) {
+          case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+          case 'storage/canceled':
+            // User canceled the upload
+            break;
+          // ...
+          case 'storage/unknown':
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
       () => {
         setIsUploading(false);
         // Upload completed successfully, now we can get the download URL
